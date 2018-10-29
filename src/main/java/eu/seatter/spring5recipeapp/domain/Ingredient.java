204a9d989900337b4d935658bb4b4e83483a63aa
james seatter <james.seatter@hotmail.co.uk>
2018-10-29 13:09:00 +0100

    Added POJO for UnitOfMeasure and setup JPA.

--- src/main/java/eu/seatter/spring5recipeapp/domain/Ingredient.java
+++ src/main/java/eu/seatter/spring5recipeapp/domain/Ingredient.java
@@ -14,8 +14,9 @@ public class Ingredient {
 
     private String description;
     private BigDecimal amount;
-    // todo
-    //private UnitOfMeasure uom;
+
+    @OneToOne(fetch=FetchType.EAGER, mappedBy="description")
+    private UnitOfMeasure uom;
 
     @ManyToOne
     private Recipe recipe;
@@ -50,5 +51,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }
