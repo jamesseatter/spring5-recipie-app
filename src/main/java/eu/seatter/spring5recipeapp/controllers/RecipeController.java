acfae0cdb80c2d280c9399ecc85623b90a6f6b1a
james seatter <james.seatter@hotmail.co.uk>
2019-03-11 17:26:06 +0100

    Added 404error.html page Added ExceptionHandler for 404 errors to RecipeController

--- src/main/java/eu/seatter/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/eu/seatter/spring5recipeapp/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package eu.seatter.spring5recipeapp.controllers;
 
 import eu.seatter.spring5recipeapp.commands.RecipeCommand;
+import eu.seatter.spring5recipeapp.exceptions.NotFoundException;
 import eu.seatter.spring5recipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by IntelliJ IDEA.
@@ -58,5 +61,17 @@ public class RecipeController {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound() {
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
     }
 }
